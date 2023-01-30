045db6cec21fcce540867b6a04ea0ccef74e5482
Aryanxfa <aryan200152@gmail.com>
2023-01-30 12:07:14 +0530

    add div in about page

--- src/routes/Home/About.jsx
+++ src/routes/Home/About.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export function About() {
-    return <h1>This is a About page</h1>
+    return (
+        <div className="login">
+            <h1>This is a About page</h1>
+        </div >
+    )
 }
